e0d3ff6e150c5dea378082750dc754754f360a54
yushichenchen <yushi2580@gmail.com>
2019-12-26 09:39:17 +0800

    12/26

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@2
+M=0
+(FUCK)
+@0
+D=M
+@OUT
+D;JLE
+@1
+D=M
+@2
+M=D+M
+@0 
+M=M-1
+D=M
+@FUCK
+D;JGT
+(OUT)
+@OUT
+0;JMP
+
+
+
+
